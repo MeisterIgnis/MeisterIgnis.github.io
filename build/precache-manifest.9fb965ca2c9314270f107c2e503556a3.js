7aacb749facadea493ccfafb9021609b2ee5d893
MeisterIgnis <58249942+MeisterIgnis@users.noreply.github.com>
2020-03-24 08:59:33 +0100

    remove /build from gitignore

---
+++ build/precache-manifest.9fb965ca2c9314270f107c2e503556a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd33720229217d830d1aa9b16b26025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5038c04139a7df55adf8",
+    "url": "/static/css/main.e48ca631.chunk.css"
+  },
+  {
+    "revision": "317c7e54a387611b719b",
+    "url": "/static/js/2.be0eaf3f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.be0eaf3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5038c04139a7df55adf8",
+    "url": "/static/js/main.b344307d.chunk.js"
+  },
+  {
+    "revision": "395f2f7745cc83063ab9",
+    "url": "/static/js/runtime-main.6525b954.js"
+  }
+]);
